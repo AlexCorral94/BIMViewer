51a20c11fd6624f760bce2649aeae12789431091
vnoves <novesvalentin@gmail.com>
2020-07-09 17:28:14 -0300

    Added possibilito to delete elements

---
+++ Js/Properties.js
@@ -0,0 +1,17 @@
+
+
+
+function addControlGui(Object3D) {
+
+    var gui = new dat.GUI();
+    var f1 = gui.addFolder('Configuration');
+    var params = {
+                intensity: spotLight.intensity,
+            };
+    f1.add( params, 'intensity', 0, 2 ).onChange( function ( val ) {
+                spotLight.intensity = val;
+                render();
+            } );
+  
+    gui1_X = gui.add(Object3D, 'Name').listen();
+}
