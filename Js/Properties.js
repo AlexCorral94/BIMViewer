0ea3d246b145e70f9daf79e13c93260f6df69e1d
vnoves <novesvalentin@gmail.com>
2020-07-10 19:56:02 -0300

    Added change colour on pick

--- Js/Properties.js
+++ Js/Properties.js
@@ -1,14 +1,26 @@
 function addControlGui() {
 
+    var colorPicker = new ColourPicker();
+
     var gui = new dat.GUI();
-    var f1 = gui.addFolder('Configuration');
+    // var f1 = gui.addFolder('Configuration');
     var params = {
                 intensity: spotLight.intensity,
             };
-    f1.add( params, 'intensity', 0, 2 ).onChange( function ( val ) {
+    gui.add( params, 'intensity', 0, 2 ).onChange( function ( val ) {
                 spotLight.intensity = val;
                 render();
             } );
   
     gui1_X = gui.add(Object3D, 'Name').listen();
+
+    gui.addColor(colorPicker, 'color0')
+    .onChange( function() {  ChangeColor(objectSel, colorPicker.color0); } );
+
 }
+
+var ColourPicker = function() {
+
+    this.color0 = "#ffae23"; // CSS string
+
+};
